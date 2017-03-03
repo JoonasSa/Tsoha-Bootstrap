e8087ff684458a79e962d54ff3bed587254574ba
JoonasSa <joonas.sarapalo@gmail.com>
2017-03-03 19:40:26 +0200

    new seed

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,8 +1,7 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
 INSERT INTO Kayttaja(username, password, admin, teacher)
 VALUES ('Testi Jäbä', 'salasana', true, true), ('Lotta Suominen', 'salakala', false, true), ('Hermanni Pallovasara', 'spurdo', false, true),
 ('Banjo', 'Kemppainen', false, true), ('Matti Niemi', 'hunter2', false, false), ('Pekka Mutka', 'spooky123', false, false),
-('Vladimir', 'Putti', false, false), ('Björje', 'Burje', false, false), ('Viivi', 'Vatukka', false, false), ('Liisi', 'Riisipiisi', false, false);
+('Björje Burje', 'Burjo', false, false), ('Liisi Riisipiisi', 'Riisipiisi', false, false);
 
 
 INSERT INTO Kurssi(nimi, opintopisteet, kuvaus) 
@@ -22,32 +21,27 @@ VALUES ('Testi', 'Jäbä', true, 1),
 ('Banjo', 'Kemppainen', false, 4);
 
 INSERT INTO Oppilas(etunimi, sukunimi, opintopisteet, opiskelijanumero)
-VALUES ('Matti', 'Niemi', 2, 5),
-('Pekka', 'Mutka', 89, 6),
-('Vladimir', 'Putti', 122, 7),
-('Björje', 'Burje', 61, 8),
-('Viivi', 'Vatukka', 74, 9),
-('Liisi', 'Riisipiisi', 0, 10);
+VALUES ('Matti', 'Niemi', 9, 5),
+('Pekka', 'Mutka', 4, 6),
+('Björje', 'Burje', 10, 7),
+('Liisi', 'Riisipiisi', 5, 8);
 
--- uusi
 INSERT INTO Toteutus(periodi, alkupvm, koepvm, info, vastuu_id, kurssi_id) 
-VALUES (2, NOW() + '1 year'::INTERVAL * ROUND(RANDOM() * 1), NOW() + '1 year'::INTERVAL * (ROUND(RANDOM() * 1) + 1), 'Helppo kurssi', 1, 1),
-(3, NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 'Paha kurssi', 2, 2),
-(4, NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 'Sellanen', 1, 2),
-(1, NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 'jAHAS', 2, 1),
-(2, NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 'Rip', 2, 3);
+VALUES (2, '2017-03-03', '2017-05-15', 'Ostakaa kurssi kirja hyvissä ajoin.', 1, 1),
+(3, '2017-01-15', '2017-03-16', 'Boolean = 50/50.', 2, 2),
+(4, '2017-03-05', '2017-05-15', 'Logiikka 1 jatkokurssi.', 1, 3),
+(1, '2017-09-04', '2017-10-22', 'Titokone, voi pojat!', 3, 4),
+(2, '2017-11-15', '2017-12-21', 'Kuljetuskerroksen kautta.', 2, 5),
+(2, '2017-11-14', '2017-12-19', 'Rip', 3, 6),
+(2, '2017-11-12', '2018-03-11', 'Syventävä kurssi. Esitietovaatimus: 100 op matikkaa.', 1, 7),
+(2, '2017-11-13', '2017-12-22', '( ͡° ʖ ͡°)', 4, 8);
 
--- uusi
---INSERT INTO Ilmoittautuminen(tote_id, ilmoittautuja) 
---VALUES (1, 4), (2, 4), (2, 5), (4, 5), (3, 4), (5, 5), (5, 4);
+INSERT INTO Ilmoittautuminen(tote_id, ilmoittautuja) 
+VALUES (1, 5), (1, 7), (2, 5), (2, 6), (2, 8), (3, 5), (4, 5), 
+(4, 7), (4, 8), (5, 6), (5, 7), (5, 8), (6, 7), (6, 6), (7, 7), (7, 8),
+(7, 6), (8, 5);
 
--- uusi
---INSERT INTO Suoritus(pvm, arvosana, tote_id, suorittaja) 
---VALUES (NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 4, 1, 4),
---(NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 1, 2, 4),
---(NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 3, 3, 4),
---(NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 2, 1, 5),
---(NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 3, 2, 5),
---(NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 4, 4, 5),
---(NOW() - '1 year'::INTERVAL * ROUND(RANDOM() * 10), 5, 5, 5);
-
+INSERT INTO Suoritus(pvm, arvosana, tote_id, suorittaja) 
+VALUES ('2017-05-15', 4, 1, 5), ('2017-03-16', 3, 2, 5),
+('2017-03-16', 4, 2, 6), ('2017-05-15', 2, 5, 7), ('2017-05-15', 5, 7, 7),
+('2017-05-15', 3, 4, 8);
